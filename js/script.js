003f4410ef3f6c318dc2e950aaf8f216dc1a8fd0
Ilya_Grin <47398274+Ilya-Grin@users.noreply.github.com>
2021-12-01 11:43:08 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const sections = document.querySelectorAll('.section');
 const buttonStart = document.querySelector('.button__start');
+const inputs = document.querySelectorAll('.input__block');
 let counter = 0;
+
+console.log(inputs);
 
 const cardNext = () => {
 	if (counter >= 4) {
 		return false;
 	} else {
 		counter++;
+		console.log(counter);
 
 		sections.forEach(e => {
 			e.style.display = 'none';
@@ -15,6 +19,12 @@ const cardNext = () => {
 		sections[counter].style.display = 'flex';
 	}
 }
+
+inputs.forEach(e => {
+	e.onclick = () => {
+		cardNext();
+	}
+});
 
 const cardBack = () => {
 	if (counter <= 0) {
